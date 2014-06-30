bf19665d019ae4ed4213be7fe32385bde75eee9b
Andres Arslanian <andres.arslanian@gmail.com>
2014-06-30 18:13:32 -0400

    project index added

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Hello World</title>
+  <!-- Bootstrap core CSS -->
+  <link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+  <p>
+       Today is <?php echo date('l F jS'); ?> and 
+       you are visiting <?php echo $_SERVER['SERVER_NAME']?>.
+  </p>
+
+  <script type="text/javascript" src="/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
